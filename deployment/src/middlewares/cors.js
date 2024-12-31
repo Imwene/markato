16d16e2883e03ddb1a1422d4f7d1387ee57c4cab
Mehari Daniel <mehash6@gmail.com>
2024-12-30 18:46:22 -0800

    updated cors for prod

--- deployment/src/middlewares/cors.js
+++ deployment/src/middlewares/cors.js
@@ -1,7 +1,7 @@
 // src/middlewares/cors.js
 const corsOptions = {
   origin: process.env.NODE_ENV === 'production' 
-    ? process.env.FRONTEND_URL 
+    ? [process.env.FRONTEND_URL, process.env.FRONTEND_URL2] 
     : ['http://localhost:5173', 'http://127.0.0.1:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
