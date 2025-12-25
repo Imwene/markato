7971f7c0c9552c96b0408692918777c2aed192c7
Mehari Daniel <mehash6@gmail.com>
2025-12-25 11:27:08 -0800

    feat(cors): add Coolify deployment URL to allowed origins
    - Add FRONTEND_URL4 environment variable support
    - Add Coolify client URL to development whitelist

--- Server/src/middlewares/cors.js
+++ Server/src/middlewares/cors.js
@@ -6,6 +6,7 @@ const corsOptions = {
           process.env.FRONTEND_URL,
           process.env.FRONTEND_URL2,
           process.env.FRONTEND_URL3,
+          process.env.FRONTEND_URL4
         ]
       : [
           "http://localhost:5173",
@@ -13,6 +14,7 @@ const corsOptions = {
           "http://127.0.0.1:5173",
           "http://127.0.0.1:4173",
           "https://petrogenetic-danae-inexact.ngrok-free.dev", // Your permanent ngrok domain
+          "https://ngs4kwk0gg0o0sog8w4wkwg8.mehariberaki.com", //coolify depoyment
           /^https:\/\/.*\.ngrok-free\.app$/, // Allow any ngrok-free.app subdomain
           /^https:\/\/.*\.ngrok-free\.dev$/, // Allow any ngrok-free.dev subdomain
         ], // Allow all origins in development since Vite proxy handles this
